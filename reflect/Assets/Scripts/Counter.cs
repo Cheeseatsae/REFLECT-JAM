9abd9f9400876d7c05031a832e0384f3c150e83a
leg <1011998@student.sae.edu.au>
2019-09-09 16:14:35 +0800

    Auto stash before merge of "master" and "origin/master"

---
+++ reflect/Assets/Scripts/Counter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Counter : MonoBehaviour
+{
+    public GameObject Text;
+    public float Playerscore =0;
+    public float currentDisplayScore = 0;
+    public bool Playerhit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //write update player hit function
+    }
+    public void foodhit()
+    {
+        if (Playerhit == true)
+        {
+            Playerscore++;
+          
+        }
+       
+    }
+}
